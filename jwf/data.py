81408a38edef62790237f5510840df89c9d315ff
yashas27 <yashas.2021@gmail.com>
2020-04-22 16:25:10 +0530

    Move data download to jwf package

---
+++ jwf/data.py
@@ -0,0 +1,31 @@
+import os
+from urllib.request import urlretrieve
+
+import pandas as pd
+
+FREMONT_URL= 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessTpe=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+
+    """Download and cache the fremont data
+
+    Parameters
+    ----------
+    filename : string (optional)
+         location to save the data
+    url : string (optional)
+         web location of the data
+    force_download : bool (optional)
+         if True, force redownload of data 
+    
+    Returns
+    -------
+    data: pandas.DataFrame
+        The fremont bridge data
+    """
+ 
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, 'Fremont.csv')
+    data=pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data.columns = ['Total','East','West']
+    return data
