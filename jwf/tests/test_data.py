59857c820a1dc595cf55902278d95d3331a50412
yashas27 <yashas.2021@gmail.com>
2020-04-23 22:01:44 +0530

    add pytest framework

---
+++ jwf/tests/test_data.py
@@ -0,0 +1,8 @@
+from jwf.data import get_fremont_data
+import pandas as pd
+
+def test_fremont_data():
+    data = get_fremont_data()
+    assert all(data.columns == ['Total', 'East', 'West'])
+    assert isinstance(data.index, pd.DatetimeIndex)
+
